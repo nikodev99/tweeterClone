b652e8c24bc065dba4d320c2f85b8a7b04139ff5
nikodev99 <nikhe.niama99@gmail.com>
2022-12-23 14:51:49 +0100

    Rest API response init

--- tweets/views.py
+++ tweets/views.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponse, Http404
+from django.http import HttpResponse, Http404, JsonResponse
 from django.shortcuts import render
 
 from .models import Tweet
@@ -10,8 +10,25 @@ def home_page_view(request, *args, **kwargs):
 
 
 def tweet_detail_view(request, tweet_id, *args, **kwargs):
+    """
+    REST API VIEW
+    consume by JavaScript/Swift/Java/iOS/Android
+    return json data
+    :param request:
+    :param tweet_id: The id of the tweet to retrieve
+    :param args:
+    :param kwargs:
+    :return: JsonResponse
+    """
+    data = {
+        "status": 200,
+        "id": tweet_id,
+        # "image_path": obj.image.url
+    }
     try:
         obj = Tweet.objects.get(id=tweet_id)
+        data["content"] = obj.content
     except:
-        raise Http404
-    return HttpResponse(f"<h1>{obj.content}</h1>")
+        data["message"] = "Not found"
+        data["status"] = 400
+    return JsonResponse(data, status=data["status"])  # json.dumps content_type='application/json'
